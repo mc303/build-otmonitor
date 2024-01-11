a37727b1ea4b15dc84b0553f179f64aec878398d
mc303 <daniel@buonocore.nl>
2024-01-11 10:49:40 +0100

    added variable

--- otgwbuildx.sh
+++ otgwbuildx.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 DOCKER_TAG=$1
-DOCKER_REPO="mc303/otmonitor:${DOCKER_TAG}"
+#DOCKER_REPO="ghcr.io/mc303/otmonitor:${DOCKER_TAG}"
+DOCKER_REPO=$2
 DOCKER_FILE="Dockerfile.otgw"
 BUIILDX_REPO='build-otgw'
-BUILD_PLATFORM="linux/amd64,linux/arm64"
+#BUILD_PLATFORM="linux/amd64,linux/arm64,linux/arm/v7"
+BUILD_PLATFORM=$3
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
@@ -13,7 +15,7 @@ docker buildx create --platform=${BUILD_PLATFORM} --name ${BUIILDX_REPO}
 docker buildx use ${BUIILDX_REPO}
 
 #build multiplatform docker image
-docker buildx build --platform=${BUILD_PLATFORM} -f ${DOCKER_FILE} -t ${DOCKER_REPO} --push .
+docker buildx build --platform=${BUILD_PLATFORM} -f ${DOCKER_FILE} -t ${DOCKER_REPO}:${DOCKER_TAG} --push .
 
 #remove multiplatform environment
-docker buildx rm ${BUIILDX_REPO
+docker buildx rm ${BUIILDX_REPO}
