e385390b610db1661e79dc348445da8033dbc677
mc303 <daniel@buonocore.nl>
2024-01-11 11:48:53 +0100

    out path added /data

--- build-otmonitor-all.sh
+++ build-otmonitor-all.sh
@@ -15,23 +15,23 @@ curl ${DOWNLOADURL}/sdx-20110317.kit --output sdx.kit
 # curl https://otgw.tclcode.com/download/tclkit${PLATFORM}  --output tclkit${PLATFORM}
 # curl https://otgw.tclcode.com/download/tclkit${PLATFORM}  --output tclkit${PLATFORM}
 
-#curl ${DOWNLOADURL}/tclkit --output tclkit
+curl ${DOWNLOADURL}/tclkit --output tclkit
 curl ${DOWNLOADURL}/tclkit-x64 --output tclkit-x64
 curl ${DOWNLOADURL}/tclkit-ahf --output tclkit-ahf
 curl ${DOWNLOADURL}/tclkit-aarch64 --output tclkit-aarch64
-#curl ${DOWNLOADURL}/tclkit.exe --output tclkit.exe
+curl ${DOWNLOADURL}/tclkit.exe --output tclkit.exe
 
-#cp tclkit /usr/local/bin/tclkit
+cp tclkit /usr/local/bin/tclkit
 cp tclkit-x64 /usr/local/bin/tclkit-x64
 cp tclkit-ahf /usr/local/bin/tclkit-ahf
 cp tclkit-aarch64 /usr/local/bin/tclkit-aarch64
-#cp tclkit.exe /usr/local/bin/tclkit.exe
+cp tclkit.exe /usr/local/bin/tclkit.exe
 #chmod +x /usr/local/bin/tclkit
 chmod +x /usr/local/bin/tclkit-x64
 
-#tclkit-x64 sdx.kit wrap otmonitor -vfs otmonitor.vfs -runtime tclkit
-tclkit-x64 sdx.kit wrap otmonitor-x64 -vfs otmonitor.vfs -runtime tclkit-x64
-tclkit-x64 sdx.kit wrap otmonitor-ahf -vfs otmonitor.vfs -runtime tclkit-ahf
-tclkit-x64 sdx.kit wrap otmonitor-aarch64 -vfs otmonitor.vfs -runtime tclkit-aarch64
-#tclkit-x64 sdx.kit wrap otmonitor.exe -vfs otmonitor.vfs -runtime tclkit.exe
+tclkit-x64 sdx.kit wrap /data/otmonitor -vfs otmonitor.vfs -runtime tclkit
+tclkit-x64 sdx.kit wrap /data/otmonitor-x64 -vfs otmonitor.vfs -runtime tclkit-x64
+tclkit-x64 sdx.kit wrap /data/otmonitor-ahf -vfs otmonitor.vfs -runtime tclkit-ahf
+tclkit-x64 sdx.kit wrap /data/otmonitor-aarch64 -vfs otmonitor.vfs -runtime tclkit-aarch64
+tclkit-x64 sdx.kit wrap /data/otmonitor.exe -vfs otmonitor.vfs -runtime tclkit.exe
 
